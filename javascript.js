4f33be5532f5b26c41ec4af1c51f2adef8a3a1b6
anny-yanting <any20102010@gmail.com>
2020-12-03 17:59:02 +0800

    modify styles and do not change element

--- javascript.js
+++ javascript.js
@@ -2,8 +2,12 @@ var ans; //紀錄答案
 var counter = 0; //紀錄作答題數
 var score = 0; //分數
 
-$('.option').click(answerQuestion);
-$('#next').click(showQuestion);
+$(document).ready(function(){
+  $('.option').click(answerQuestion);
+});
+$(document).ready(function(){
+  $('.nextone').click(answerQuestion);
+});
 
 //顯示題目
 function showQuestion(){
@@ -11,36 +15,46 @@ function showQuestion(){
   var q = questions.splice(random,1)[0];
 
   counter++;
-  $("#counter").text("Question: " + counter);
-  $("#content").text(q.content);
-  $("#a").text(q.a);
-  $("#b").text(q.b);
-  $("#c").text(q.c);
-  $("#d").text(q.d);
-  ans = q.ans;
-  $(".option").removeClass("red");
-  $(".option").removeClass("green");
-  $("#next").hide();
+  $(document).ready(function(){
+    $("#counter").text("Question: " + counter);
+    $("#content").text(q.content);
+    $("#a").text(q.a);
+    $("#b").text(q.b);
+    $("#c").text(q.c);
+    $("#d").text(q.d);
+    ans = q.ans;
+    $(".option").removeClass("gray");
+    $(".option").removeClass("green");
+    $(".nextone").hide();
+  });
 }
 //檢查答案
 function answerQuestion(){
   var playerans = $(this).attr("id");
   if(playerans == ans){
     score+=10;
-    $(this).addclass("green");
+    $(document).ready(function(){
+      $(this).addclass("green");
+    });
+    
   }else{
-    $(this).addclass("red");
+    $(document).ready(function(){
+      $(this).addclass("gray");
+    });
   }
   if(counter>=10){
     Gameover();
   }else{
-    $('#next').show();
+    $(document).ready(function(){
+      $('.nextone').show();
+    });
   }
 }
 
 //遊戲結束
 function Gameover(){
-
+  
 }
 
   
+
