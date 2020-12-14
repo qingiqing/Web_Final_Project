f84b6fafd065c4b4e2139935388ea9fce710b36a
lintsaihsin <mimmylintw@gmail.com>
2020-12-14 13:27:46 +0800

    new

--- javascript.js
+++ javascript.js
@@ -56,5 +56,23 @@ function Gameover(){
   
 }
 
-  
-
+$(document).ready(function(){
+    $("#button1").click(function(){
+      $("#p1").fadeToggle("slow");
+    });
+    $("#button2").click(function(){
+      $("#p2").fadeToggle("slow");
+    });
+    $("#button3").click(function(){
+      $("#p3").fadeToggle("slow");
+    });
+    $("#example").click(function(){
+      $("#gif").fadeToggle("slow");
+    });
+    $("#button4").click(function(){
+      $("#p4").fadeToggle("slow");
+    });
+    $("#button5").click(function(){
+      $("#p5").fadeToggle("slow");
+    });
+  });
